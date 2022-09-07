c619bfcd0613fec3a90bf363325c7811206f5aa5
jahlonteaching <jehincapie@udemedellin.edu.co>
2022-09-06 23:21:01 -0500

    Add utility class to encrypt

---
+++ MoviesManager/Util/Encryptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MoviesManager.Util
+{
+    public class Encryptor
+    {
+        public static string SHA1Hash(string text)
+        {
+            SHA1 sha1 = SHA1.Create();
+
+            // Compute hash from the bytes of text
+            byte[] hash = sha1.ComputeHash(ASCIIEncoding.UTF8.GetBytes(text));
+
+            
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                strBuilder.Append(hash[i].ToString("X2"));
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
